85c6af2d7163b2dd09fe14aa7b62a96df5ea0c00
Lastv25 <albertogiovanni78@gmail.com>
2020-07-29 18:54:17 +0200

    test __init__ file

--- MyFirstAddOn/__init__.py
+++ MyFirstAddOn/__init__.py
@@ -1,61 +1,45 @@
-
-'''
-hugo
-'''
-
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
 
 bl_info = {
-    "name":        "My First Add On",
-    "description": "Test Add On.",
-    "author":      "Hugo Monjoux",
-    "version":     (0, 0, 1),
-    "blender":     (2, 83, 0),
-    "warning":     "This version is still in development."
+    "name": "My First Add On",
+    "author": "Hugo Monjoux",
+    "version": (0, 0, 1),
+    "blender": (2, 80, 0),
+    "description": "fdktdytdjfxdc",
 }
 
-
-
-# Test Environment
-##################################
-
+import bpy
 import os
-import sys
-import traceback
-from os.path import dirname, join, abspath, basename
-currentDirectory = dirname(abspath(__file__))
-addonsDirectory = dirname(currentDirectory)
-compilationInfoPath = join(currentDirectory, "compilation_info.json")
-addonName = basename(currentDirectory)
 
+from MyFirstAddOn.MyFirstTree import *
 
-if addonName != "MyFirstAddOn":
-    message = ("\n\n"
-        "The name of the folder containing this addon has to be 'MyFirstAddOn'.\n"
-        "Please rename it.")
-    raise Exception(message)
-
-
-from MyFirstAddOn.auto_load import *
-
-if "auto_load" not in globals():
-    message = ("\n\n"
-        "The Animation Nodes addon cannot be registered correctly.\n"
-        "Please try to remove and install it again.\n"
-        "If it still does not work, report it.\n")
-    raise Exception(message)
-
-# register
-##################################
-
-from . import auto_load
-auto_load.init()
-
-
+# =========================================================================
+# Registration:
+# =========================================================================
 
 def register():
-    auto_load.register()
-    print("Registered Animation Nodes")
+    MyFirstTree.register()
+
 
 def unregister():
-    auto_load.unregister()
-    print("Unregistered Animation Nodes")
+    MyFirstTree.unregister()
+
+
+if __name__ == "__main__":
+    register()
