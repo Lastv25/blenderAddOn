863640c731bb10ba8aad1f39362ef82fc3d3721b
Lastv25 <albertogiovanni78@gmail.com>
2020-07-29 17:47:03 +0200

    test __init__ file and creation of auto_load file

--- MyFirstAddOn/__init__.py
+++ MyFirstAddOn/__init__.py
@@ -1,15 +1,29 @@
+
 '''
-author: Monjoux Hugo
-date:
+Copyright (C) 2017 Jacques Lucke
+mail@jlucke.com
+Created by Jacques Lucke
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 
 bl_info = {
-    "name":        "My First Add On",
-    "description": "addOn to experiment the add On development",
-    "author":      "Hugo Monjoux",
-    "version":     (0, 0, 1),
+    "name":        "Animation Nodes",
+    "description": "Node based visual scripting system designed for motion graphics in Blender.",
+    "author":      "Jacques Lucke",
+    "version":     (2, 1, 7),
     "blender":     (2, 80, 0),
+    "location":    "Animation Nodes Editor",
+    "category":    "Node",
     "warning":     "This version is still in development."
 }
 
@@ -33,9 +47,9 @@ if addonName != "MyFirstAddOn":
         "The name of the folder containing this addon has to be 'MyFirstAddOn'.\n"
         "Please rename it.")
     raise Exception(message)
+from MyFirstAddOn.auto_load import *
 
-
-try: from . import auto_load
+try: pass
 except: pass
 
 if "auto_load" not in globals():
@@ -51,11 +65,11 @@ if "auto_load" not in globals():
 from . import auto_load
 auto_load.init()
 
+
 def register():
     auto_load.register()
-    print("Registered My First Add On")
+    print("Registered Animation Nodes")
 
 def unregister():
     auto_load.unregister()
-    print("Unregistered My First Add On")
-
+    print("Unregistered Animation Nodes")
--- MyFirstAddOn/auto_load.py
+++ MyFirstAddOn/auto_load.py
@@ -1,3 +1,4 @@
+
 import os
 import bpy
 import sys
@@ -16,12 +17,14 @@ __all__ = (
 modules = None
 ordered_classes = None
 
+
 def init():
     global modules
     global ordered_classes
 
     modules = get_all_submodules(Path(__file__).parent)
     ordered_classes = get_ordered_classes_to_register(modules)
+
 
 def register():
     for cls in ordered_classes:
@@ -32,6 +35,7 @@ def register():
             continue
         if hasattr(module, "register"):
             module.register()
+
 
 def unregister():
     for cls in reversed(ordered_classes):
@@ -50,9 +54,11 @@ def unregister():
 def get_all_submodules(directory):
     return list(iter_submodules(directory, directory.name))
 
+
 def iter_submodules(path, package_name):
     for name in sorted(iter_submodule_names(path)):
         yield importlib.import_module("." + name, package_name)
+
 
 def iter_submodule_names(path, root=""):
     for _, module_name, is_package in pkgutil.iter_modules([str(path)]):
@@ -70,18 +76,21 @@ def iter_submodule_names(path, root=""):
 def get_ordered_classes_to_register(modules):
     return toposort(get_register_deps_dict(modules))
 
+
 def get_register_deps_dict(modules):
     my_classes = set(iter_my_classes(modules))
-    my_classes_by_idname = {cls.bl_idname : cls for cls in my_classes if hasattr(cls, "bl_idname")}
+    my_classes_by_idname = {cls.bl_idname: cls for cls in my_classes if hasattr(cls, "bl_idname")}
 
     deps_dict = {}
     for cls in my_classes:
         deps_dict[cls] = set(iter_my_register_deps(cls, my_classes, my_classes_by_idname))
     return deps_dict
 
+
 def iter_my_register_deps(cls, my_classes, my_classes_by_idname):
     yield from iter_my_deps_from_annotations(cls, my_classes)
     yield from iter_my_deps_from_parent_id(cls, my_classes_by_idname)
+
 
 def iter_my_deps_from_annotations(cls, my_classes):
     for value in typing.get_type_hints(cls, {}, {}).values():
@@ -90,11 +99,13 @@ def iter_my_deps_from_annotations(cls, my_classes):
             if dependency in my_classes:
                 yield dependency
 
+
 def get_dependency_from_annotation(value):
     if isinstance(value, tuple) and len(value) == 2:
         if value[0] in (bpy.props.PointerProperty, bpy.props.CollectionProperty):
             return value[1]["type"]
     return None
+
 
 def iter_my_deps_from_parent_id(cls, my_classes_by_idname):
     if bpy.types.Panel in cls.__bases__:
@@ -104,12 +115,14 @@ def iter_my_deps_from_parent_id(cls, my_classes_by_idname):
             if parent_cls is not None:
                 yield parent_cls
 
+
 def iter_my_classes(modules):
     base_types = get_register_base_types()
     for cls in get_classes_in_modules(modules):
         if any(base in base_types for base in cls.__bases__):
             if not getattr(cls, "is_registered", False):
                 yield cls
+
 
 def get_classes_in_modules(modules):
     classes = set()
@@ -118,10 +131,12 @@ def get_classes_in_modules(modules):
             classes.add(cls)
     return classes
 
+
 def iter_classes_in_module(module):
     for value in module.__dict__.values():
         if inspect.isclass(value):
             yield value
+
 
 def get_register_base_types():
     return set(getattr(bpy.types, name) for name in [
@@ -146,5 +161,5 @@ def toposort(deps_dict):
                 sorted_values.add(value)
             else:
                 unsorted.append(value)
-        deps_dict = {value : deps_dict[value] - sorted_values for value in unsorted}
+        deps_dict = {value: deps_dict[value] - sorted_values for value in unsorted}
     return sorted_list
